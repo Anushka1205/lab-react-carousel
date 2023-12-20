d0437ad94b733181abb80725c06d3ce9f3133a44
your_name_here <your_email@email.com>
2023-12-20 09:43:29 +0530

    added

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -7,7 +7,52 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 // implement the class below
 class Carousel extends Component {
+
+    constructor(){
+        super()
+
+        this.state={
+            index:0
+        }
+    }
   
+    handleBackArrow=()=>{
+        if(this.state.index==0){
+            this.setState({
+                index:images.length-1
+            })
+        }else{
+            this.setState({
+                index:this.state.index-1
+            })
+        }
+        
+    }
+
+    handleForwardArrow=()=>{
+        if(this.state.index==images.length-1){
+            this.setState({
+                index:0
+            })
+        }else if(this.state.index<images.length-1){
+            this.setState({
+                index:this.state.index+1
+            })
+        }
+        
+    }
+
+    render(){
+        return(
+            <>
+                <img className="carousel-img" src={images[this.state.index].img} alt=""/>
+                <h1 className="carousel-title">{images[this.state.index].title}</h1>
+                <h2 className="carousel-sub-title">{images[this.state.index].subtitle}</h2>
+                <ArrowBackIosIcon className="box Back-arr" onClick={this.handleBackArrow}/>
+                <ArrowForwardIosIcon className="box Front-arr" onClick={this.handleForwardArrow}/>
+            </>
+        )
+    }
 }
 
 export default Carousel;
